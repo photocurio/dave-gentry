cd7ee5596c10f8ea9e34be5e978a5657f0a604aa
Peter Mumford <sunraydust@gmail.com>
2025-05-22 06:31:04 -0400

    added boat nav include

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,24 @@
 const sitemap = require( "@quasibit/eleventy-plugin-sitemap" )
-const sass = require("sass");
-const path = require("path");
+const sass = require( "sass" )
+const path = require( "path" )
 
 
 module.exports = ( config ) => {
 	config.addPassthroughCopy( 'src/assets' )
 	config.addPassthroughCopy( 'src/pics' )
-	config.addTemplateFormats("scss");
-    config.addExtension("scss", {
-        outputFileExtension: "css",
-        compile: async function(inputContent, inputPath) {
-            let parsed = path.parse(inputPath);
-            if (parsed.name.startsWith("_")) return;
+	config.addTemplateFormats( "scss" )
+	config.addExtension( "scss", {
+		outputFileExtension: "css",
+		compile: async function ( inputContent, inputPath ) {
+			let parsed = path.parse( inputPath )
+			if ( parsed.name.startsWith( "_" ) ) return
 
-            return async () => {
-                let result = sass.compile(inputPath);
-                return result.css;
-            };
-        }
-    });
+			return async () => {
+				let result = sass.compile( inputPath )
+				return result.css
+			}
+		}
+	} )
 
 
 	// Add the sitemap plugin.
