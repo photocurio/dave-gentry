81c4cd3bc47ba30fa2fc611b5fa99cc8007dad0a
Peter Mumford <sunraydust@gmail.com>
2025-06-04 06:33:52 -0400

    changes pic to images in eleventyconfig

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ const path = require( "path" )
 
 
 module.exports = ( config ) => {
-	config.addPassthroughCopy( 'src/pics' )
+	config.addPassthroughCopy( 'src/images' )
 	config.addPassthroughCopy( 'src/admin' )
 	config.addTemplateFormats( "scss" )
 	config.addExtension( "scss", {
