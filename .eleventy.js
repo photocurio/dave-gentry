c3779b23835d8d12775350a5f4cdbaf7c55c99f1
photocurio <sunraydust@gmail.com>
2025-06-06 19:04:39 -0400

    formatting

--- .eleventy.js
+++ .eleventy.js
@@ -4,18 +4,17 @@ const path = require( 'path' )
 
 module.exports = function ( config ) {
 	config.addPassthroughCopy( 'src/images' )
-	config.addPassthroughCopy( 'src/admin' )
 	config.addPassthroughCopy( 'src/favicon.jpg' )
 	config.addTemplateFormats( 'scss' )
 	config.addExtension( 'scss', {
 		outputFileExtension: 'css',
 		useLayouts: false,
 		compile: function ( inputContent, inputPath ) {
-
 			let parsed = path.parse( inputPath )
-			
 			if ( parsed.name.startsWith( '_' ) ) return
 
+			// Eleventy expects a function that returns the compiled content
+			// This allows for deferred compilation and caching
 			return () => {
 				let result = sass.compileString(inputContent, {
 					loadPaths: [ parsed.dir ]
@@ -23,7 +22,6 @@ module.exports = function ( config ) {
 
 				return result.css;
 			};
-
 		}
 	} )
 
